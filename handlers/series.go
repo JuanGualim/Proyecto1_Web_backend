00c741a038269e3be5fc4aa326637c5ea0a1bb59
JuanGualim <gua24852@uvg.edu.gt>
2026-04-22 09:20:58 -0600

    Creació nde primer endpoint GET /series

--- handlers/series.go
+++ handlers/series.go
@@ -1,1 +1,34 @@
 package handlers
+
+import (
+	"net/http"
+
+	"series-api/database"
+	"series-api/models"
+	"series-api/utils"
+)
+
+func GetSeries(w http.ResponseWriter, r *http.Request) {
+	enableCORS(&w)
+
+	rows, err := database.DB.Query("SELECT id, name, current_episode, total_episodes, image_url FROM series")
+	if err != nil {
+		utils.Error(w, http.StatusInternalServerError, "Error fetching series")
+		return
+	}
+	defer rows.Close()
+
+	var seriesList []models.Series
+
+	for rows.Next() {
+		var s models.Series
+		err := rows.Scan(&s.ID, &s.Name, &s.CurrentEpisode, &s.TotalEpisodes, &s.ImageURL)
+		if err != nil {
+			utils.Error(w, http.StatusInternalServerError, "Error reading data")
+			return
+		}
+		seriesList = append(seriesList, s)
+	}
+
+	utils.JSON(w, http.StatusOK, seriesList)
+}
--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,11 @@
 package routes
+
+import (
+	"net/http"
+
+	"series-api/handlers"
+)
+
+func SetupRoutes() {
+	http.HandleFunc("/series", handlers.GetSeries)
+}
